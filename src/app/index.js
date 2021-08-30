151dff49f538d2b44c54e0a4830e3c4df95da02e
nauthiz-jera <nauthiz.jera@gmail.com>
2021-08-30 17:21:17 -0400

    update app

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { routes } from "../pages/routes";
 import { BootStrapper } from "@workshop/app-components";
+
+import { routes } from "../pages/routes";
 import { reducer } from "./../state";
 
 const App = () => (
