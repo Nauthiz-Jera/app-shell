beb00a0f3aa0a84d29e43c80c06d64891a62dc7b
nauthiz-jera <nauthiz.jera@gmail.com>
2021-08-30 22:07:33 -0400

    remove files from git remote mess up

--- src/root.component.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import Root from "./app";
-
-describe("Root component", () => {
-  it("should be in the document", () => {
-    const { getByText } = render(<Root name="Testapp" />);
-    expect(getByText(/Testapp is mounted!/i)).toBeInTheDocument();
-  });
-});
